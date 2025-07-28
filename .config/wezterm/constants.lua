8c1ec0b947b1904c8c47a549f8157f9fa9ac848e
Ahmad Allehyany <aallehyany@gmail.com>
2025-07-28 22:58:36 +0300

    Fixed import path for bg image

--- .config/wezterm/constants.lua
+++ .config/wezterm/constants.lua
@@ -1,5 +1,16 @@
 local M = {}
 
-M.bg_image = os.getenv("HOME") .. "/.config/wezterm/assets/bg_blurred.png"
 
+-- Determine the path separator based on the OS
+-- For POSIX (Linux, macOS), it's '/', for Windows, it's '\'
+local sep = package.config:sub(1, 1)
+
+-- Get the user's home directory in a cross-platform way
+local home = os.getenv("HOME") or os.getenv("USERPROFILE")
+
+-- Define the path components
+local path_parts = { home, ".config", "wezterm", "assets", "bg_blurred.png" }
+
+-- Concatenate the parts with the correct separator
+M.bg_image = table.concat(path_parts, sep)
 return M
