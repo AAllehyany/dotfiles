11bac564bab5ce14f14c13331a62180750646973
Ahmed Allehyany <ahmed.allehyany@intigral.net>
2025-07-29 12:21:13 +0300

    adjusted aerospace and wezterm settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local constants = require("constants")
 local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 -- font settings
-config.font_size = 16
+config.font_size = 18
 config.line_height = 1.2
 config.font = wezterm.font("MonoLisa")
 
@@ -14,8 +14,12 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   config.default_prog = { 'pwsh.exe', '-NoLogo' }
 end
 
+config.initial_cols = 100
+config.initial_rows = 80
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
 config.window_background_image = constants.bg_image
 config.color_scheme = 'ayu'
 config.inactive_pane_hsb = {
@@ -67,7 +71,71 @@ config.keys = {
    key = 'q',
    mods = 'LEADER',
    action = wezterm.action.TogglePaneZoomState
- }
+ },
+ {
+   key = 'h',
+   mods = 'ALT',
+   action = wezterm.action.ActivatePaneDirection('Left')
+ },
+
+{
+   key = 'j',
+   mods = 'ALT',
+   action = wezterm.action.ActivatePaneDirection('Down')
+ },
+ {
+   key = 'k',
+   mods = 'ALT',
+   action = wezterm.action.ActivatePaneDirection('Up')
+ },
+ {
+   key = 'l',
+   mods = 'ALT',
+   action = wezterm.action.ActivatePaneDirection('Right')
+ },
+ 
 
 }
+
+-- Additional keybindings for pane switching using indexed numbers
+
+for i = 0, 9 do
+  table.insert(config.keys, {
+    -- The key to press (e.g., "0", "1", "2")
+    key = tostring(i),
+    
+    -- The modifier key
+    mods = 'LEADER',
+    
+    -- The action to perform: activate the pane by its index 'i'
+    action = wezterm.action.ActivatePaneByIndex(i),
+  })
+end
+
+wezterm.on('update-right-status', function(window, pane)
+  local num_panes = #window:active_tab():panes()
+  local is_zoomed = false
+
+  -- Check if any pane in the current tab is zoomed
+  for _, p in ipairs(window:active_tab():panes()) do
+    if p.is_zoomed then
+      is_zoomed = true
+      break
+    end
+  end
+
+  -- Display the indicator only if a pane is zoomed and there are multiple panes
+  if is_zoomed and num_panes > 1 then
+    window:set_right_status(wezterm.format {
+      -- Add a little bit of styling to make it stand out
+      { Background = { Color = '#ffb86c' } }, -- A nice orange color
+      { Foreground = { Color = '#282a36' } }, -- A contrasting dark color
+      { Text = string.format(' 🔍 Zoomed (%d panes) ', num_panes) },
+    })
+  else
+    -- Clear the status when not zoomed
+    window:set_right_status('')
+  end
+end)
+
 return config
