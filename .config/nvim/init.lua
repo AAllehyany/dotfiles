a090b83d6166839a2b8004ebcfbf7ab0943366c9
Ahmed Allehyany <ahmed.allehyany@intigral.net>
2025-07-30 13:51:35 +0300

    setup diagnostics max width

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,40 +27,28 @@ now(function()
     overrides = {}
   })
   vim.cmd('colorscheme ayu-mirage')
+  local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+  function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+    opts = opts or {}
+    opts.border = opts.border or 'single'
+    opts.max_width = opts.max_width or 60 -- Set your desired max width here
+    return orig_util_open_floating_preview(contents, syntax, opts, ...)
+  end
+
   vim.api.nvim_create_autocmd('LspAttach', {
     desc = 'LSP actions',
     callback = function(event)
       local opts = { buffer = event.buf }
 
-      -- Display documentation of the symbol under the cursor
       vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-
-      -- Jump to the definition
-      vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-
-      -- Format current file
-      vim.keymap.set({ 'n', 'x' }, 'gq', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-
-      -- Displays a function's signature information
       vim.keymap.set('i', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-
-      -- Jump to declaration
-      vim.keymap.set('n', '<leader>gd', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-
-      -- Lists all the implementations for the symbol under the cursor
       vim.keymap.set('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-
-      -- Jumps to the definition of the type symbol
       vim.keymap.set('n', '<leader>gt', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-
-      -- Lists all the references
       vim.keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-
-      -- Renames all references to the symbol under the cursor
       vim.keymap.set('n', '<leader>gn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-
-      -- Selects a code action available at the current cursor position
       vim.keymap.set('n', '<leader>ga', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+      vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<cr>',
+        { buffer = event.buf, desc = 'Show diagnostics' })
     end,
   })
   vim.lsp.enable('lua_ls')
